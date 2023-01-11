5c58b8cf0d19b33b793c86dcf1787776f131f7e7
Mateus Garcia <mateussk83@gmail.com>
2023-01-11 07:57:27 -0300

    feat create test authenticate user and create category

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,67 @@
+import { AppError } from "../../../../erros/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    );
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("Should be able to Authenticate an User", async () => {
+    const user: ICreateUserDTO = {
+      driver_license: "000111",
+      email: "user@teste.com",
+      password: "12314",
+      name: "User Teste",
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("should not be able to authenticate an not existent user", () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "false@email.com",
+        password: "1234",
+      }); 
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to authenticate with incorrect password", () => {
+   expect(async () => {
+    const user:ICreateUserDTO = {
+     driver_license: "9999",
+     email: "user@user.com",
+     password:"1234",
+     name: "User test Error"
+    } 
+
+    await createUserUseCase.execute(user)
+
+    await authenticateUserUseCase.execute({
+     email: user.email,
+     password: "IncorrectPassword"
+    })
+
+
+
+   }).rejects.toBeInstanceOf(AppError)
+  })
+});
---
+++ src/modules/cars/useCases/createCategory/createCategoryUseCase.spec.ts
@@ -0,0 +1,55 @@
+import { AppError } from "../../../../erros/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+
+// sempre antes de algum teste ele vai fazer determinada função
+ beforeEach(() => {
+  categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+  createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory)
+ })
+
+ it("should be able to create a new category", async () => {
+  const category = {
+   name: "Category Test",
+   description: "Category description Test",
+  }
+  await createCategoryUseCase.execute({
+   name: category.name,
+   description: category.description
+  })
+
+  const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name)
+
+  console.log(categoryCreated)
+  // se a categoria for salva com sucesso ela vai receber o id
+  expect(categoryCreated).toHaveProperty("id");
+ })
+
+ it("should bot be able to create a new category with same name exists", async () => {
+  expect(async () => {
+   const category = {
+    name: "Category Test",
+    description: "Category description Test",
+   }
+   await createCategoryUseCase.execute({
+    name: category.name,
+    description: category.description
+   })
+ 
+   await createCategoryUseCase.execute({
+    name: category.name,
+    description: category.description
+    
+   })
+   // aqui ele ta falando se der um erro dentro do app error vai dar sucesso no teste
+  }).rejects.toBeInstanceOf(AppError)
+  
+ })
+
+ 
+})
