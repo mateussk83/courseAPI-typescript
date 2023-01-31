5d683eb415bac6b013c57a9e48aa1eb504c96086
Mateus Garcia <mateussk83@gmail.com>
2023-01-31 19:28:01 -0300

    feat create devolution rental

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -1,22 +1,30 @@
-import dayjs from "dayjs"
-import utc from "dayjs/plugin/utc"
-import { IDateProvider } from "../IDateProvider"
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import { IDateProvider } from "../IDateProvider";
 
-dayjs.extend(utc)
-
+dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
- dateNow():Date {
-  return dayjs().toDate()
- }
- convertToUTC(date: Date):string {
-  return dayjs(date).utc().local().format()
- }
- compareInHours(start_date: Date, end_date: Date): number {
-  const end_date_utc = this.convertToUTC(end_date);
-  const start_date_utc = this.convertToUTC(start_date);
-  return dayjs(end_date_utc).diff(start_date_utc, "hours")
- }
+ 
+  dateNow(): Date {
+    return dayjs().toDate();
+  }
+
+  convertToUTC(date: Date): string {
+    return dayjs(date).utc().local().format();
+  }
+
+  compareInHours(start_date: Date, end_date: Date): number {
+    const end_date_utc = this.convertToUTC(end_date);
+    const start_date_utc = this.convertToUTC(start_date);
+    return dayjs(end_date_utc).diff(start_date_utc, "hours");
+  }
+
+  compareInDays(start_date: Date, end_date: Date): number {
+    const end_date_utc = this.convertToUTC(end_date);
+    const start_date_utc = this.convertToUTC(start_date);
+    return dayjs(end_date_utc).diff(start_date_utc, "days");
+  }
 }
 
-export { DayjsDateProvider }
+export { DayjsDateProvider };
