c49eee87872b1657e111d2afb0a98407d0ebef73
Mateus Garcia <mateussk83@gmail.com>
2023-02-16 08:21:45 -0300

    feat insert images in the s3 AWS

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -13,6 +13,7 @@ import { AppError } from "../../errors/AppError";
 import { router } from "./routes";
 
 import swaggerFile from "../../../swagger.json"
+import upload from "../../../config/upload";
 
 createConnection()
 const app = express();
@@ -21,7 +22,8 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
+app.use("/avatar", express.static(`${upload.tmpFolder}/cars`))
 
 app.use(router)
 
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -4,14 +4,18 @@ import uploadConfig from "../../../../config/upload";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../../../../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../../../../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ProfileUserController } from "../../../../modules/accounts/useCases/profileUser/ProfileUserController";
 
 const usersRoutes = Router();
 
 const uploadAvatar = multer(uploadConfig)
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController()
 
-usersRoutes.post("/", createUserController.handle);
+usersRoutes.post("/", createUserController.handle); 
+
+usersRoutes.get("/profile",ensureAuthenticated, profileUserController.handle)
 
 usersRoutes.patch(
   "/avatar",
