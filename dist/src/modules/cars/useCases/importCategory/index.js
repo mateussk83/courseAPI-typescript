e5e63f973833f81de5283035f4f8cb2659669b48
Mateus Garcia <mateussk83@gmail.com>
2023-03-06 19:04:50 -0300

    feat insert babel in project

--- dist/src/modules/cars/useCases/importCategory/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.importCategoryController = void 0;
-const ImportCategoryController_1 = require("./ImportCategoryController");
-const ImportCategoryUseCase_1 = require("./ImportCategoryUseCase");
-const categoriesRepository = null;
-const importCategoryUseCase = new ImportCategoryUseCase_1.ImportCategoryUseCase(categoriesRepository);
-const importCategoryController = new ImportCategoryController_1.ImportCategoryController(importCategoryUseCase);
-exports.importCategoryController = importCategoryController;
--- dist/src/shared/infra/http/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-const app_1 = require("./app");
-app_1.app.listen(3333);
